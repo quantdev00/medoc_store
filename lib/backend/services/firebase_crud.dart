eacfa2c94d781dd260f0125c3d82bdbefa34f6d0
Leinad-17 <danielngolu154@gmail.com>
2023-03-10 08:21:32 +0200

    Saved firebase crud

--- lib/backend/services/firebase_crud.dart
+++ lib/backend/services/firebase_crud.dart
@@ -4,4 +4,6 @@ import 'package:drug_store/backend/response.dart';
 final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 final CollectionReference _collection = _firestore.collection("Drugs");
 
-class FirebaseCrud {}
+class FirebaseCrud {
+  
+}

8f2a712126703fe97272f8b27b51f0e0dab61c0a
Leinad-17 <danielngolu154@gmail.com>
2023-03-17 12:37:50 +0200

    Added crud operations

--- lib/backend/services/firebase_crud.dart
+++ lib/backend/services/firebase_crud.dart
@@ -5,5 +5,80 @@ final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 final CollectionReference _collection = _firestore.collection("Drugs");
 
 class FirebaseCrud {
-  
+  // Method to add drug in the db
+  static Future<Response> addDrug({
+    required String name,
+    required String description,
+    required String price,
+  }) async {
+    Response response = Response();
+    DocumentReference documentReferencer = _collection.doc();
+
+    Map<String, dynamic> data = <String, dynamic>{
+      "drug_name": name,
+      "description": description,
+      "price": price,
+    };
+
+    var result = await documentReferencer.set(data).whenComplete(() {
+      response.code = 200;
+      response.message = "Successfully added to the db";
+    }).catchError((e) {
+      response.code = 500;
+      response.message = e;
+    });
+
+    return response;
+  }
+
+  //Read Drug records
+  static Stream<QuerySnapshot> getDrugRecord() {
+    CollectionReference notesItemCollection = _collection;
+    return notesItemCollection.snapshots();
+  }
+
+  //Update drugs record method
+  static Future<Response> updateDrug({
+    required String name,
+    required String description,
+    required String price,
+    required String docId,
+  }) async {
+    Response response = Response();
+    DocumentReference documentReferencer = _collection.doc(docId);
+
+    Map<String, dynamic> data = <String, dynamic>{
+      "drug_name": name,
+      "description": description,
+      "price": price,
+    };
+
+    await documentReferencer.update(data).whenComplete(() {
+      response.code = 200;
+      response.message = "Record updated successfully in the db";
+    }).catchError((e) {
+      response.code = 500;
+      response.message = e;
+    });
+
+    return response;
+  }
+
+  //* Delete Drug record
+  static Future<Response> deleteDrug({
+    required String docId,
+  }) async {
+    Response response = Response();
+    DocumentReference documentReferencer = _collection.doc(docId);
+
+    await documentReferencer.delete().whenComplete(() {
+      response.code = 200;
+      response.message = "Deleted successfully";
+    }).catchError((e) {
+      response.code = 500;
+      response.message = e;
+    });
+
+    return response;
+  }
 }
--- lib/view/add_medoc_view.dart
+++ lib/view/add_medoc_view.dart
@@ -1,3 +1,4 @@
+import 'package:drug_store/backend/services/firebase_crud.dart';
 import 'package:drug_store/widgets/my_widgets.dart';
 import 'package:flutter/material.dart';
 
@@ -9,17 +10,116 @@ class AddDrugView extends StatefulWidget {
 }
 
 class _AddDrugViewState extends State<AddDrugView> {
+  final _drug_name = TextEditingController();
+  final _drug_description = TextEditingController();
+  final _drug_price = TextEditingController();
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
+    //*Drug name textfield config
+    final drugNameTextField = TextFormField(
+      controller: _drug_name,
+      autofocus: false,
+      validator: (value) {
+        if (value == null || value.trim().isEmpty) {
+          return 'This field is required';
+        }
+      },
+    );
+    //* Description field right here
+    final descriptionTextField = TextFormField(
+      controller: _drug_description,
+      autofocus: false,
+      validator: (value) {
+        if (value == null || value.trim().isEmpty) {
+          return 'This field is required';
+        }
+      },
+    );
+
+    //* Field get Price
+    final priceTextField = TextFormField(
+      controller: _drug_price,
+      autofocus: false,
+      validator: (value) {
+        if (value == null || value.trim().isEmpty) {
+          return 'This field is required';
+        }
+      },
+    );
+
+    //* View list button
+    final viewDrugListButton = TextButton(
+      onPressed: () {
+        Navigator.of(context).push(ListView());
+        // Navigator.pushAndRemoveUntil<dynamic>(
+        //     context,
+        //     MaterialPageRoute<dynamic>(builder: ((context) => ListPage())),
+        //     (route) => false);
+      },
+      child: const Text('View list drug list'),
+    );
+
+    final saveButton = Material(
+      elevation: 5.0,
+      child: MaterialButton(
+        onPressed: () async {
+          if (_formKey.currentState!.validate()) {
+            var response = await FirebaseCrud.addDrug(
+                name: _drug_name.text,
+                description: _drug_description.text,
+                price: _drug_price.text);
+            if (response.code != 200) {
+              showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      content: Text(response.message.toString()),
+                    );
+                  });
+            } else {
+              showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      content: Text(response.message.toString()),
+                    );
+                  });
+            }
+          }
+        },
+        child: Text(
+          "Save",
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+
     return Material(
       child: SingleChildScrollView(
         padding: const EdgeInsets.only(top: 50, left: 15, right: 20),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            appBar,
-            const TextField(),
-            const TextField(),
-            TextButton(onPressed: () {}, child: const Text("Save")),
+            Form(
+              key: _formKey,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(children: [
+                  drugNameTextField,
+                  const SizedBox(height: 25),
+                  descriptionTextField,
+                  const SizedBox(height: 35),
+                  priceTextField,
+                  const SizedBox(height: 45),
+                  saveButton,
+                  const SizedBox(height: 15),
+                ]),
+              ),
+            )
           ],
         ),
       ),
